6f15a7a9bf07d6275b5e6db98474653bc6f4196b
Eugenio Herrera <eiherrera@uc.cl>
2017-04-08 15:38:09 -0600

    the api is ready bitches

---
+++ server/app_api.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient()
+db = client.test
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/users")
+def get_users():
+    users_collection = db.users
+    users = [user for user in users_collection.find()]
+    for user in users:
+        del user["_id"]
+    return jsonify({"users": [user for user in users]})
+
+@app.route("/user/<uid>")
+def get_user(uid=None):
+    users_collection = db.users
+    user = users_collection.find_one({"id": int(uid)})
+    del user["_id"]
+    return jsonify({"user": user})
+
+@app.route('/teacher/<uid>')
+def get_teacher(uid=None):
+    teachers_collection = db.teachers
+    users_collection = db.users
+    teacher = teachers_collection.find_one({'id':int(uid)})
+    user = users_collection.find_one({"id": int(uid)})
+    teacher.update(user)
+    del teacher['_id']
+    return jsonify({'teacher': teacher})
+
+@app.route('/teachers/<sigla>')
+def get_teachers_by_course(sigla):
+    courses_collection = db.courses
+    teachers = db.teachers
+    users_collection = db.users
+    results = []
+    course = courses_collection.find_one({'sigla':sigla})
+    for teacher_id in course['teachers']:
+        teacher = teachers.find_one({'id':int(teacher_id)})
+        user = users_collection.find_one({"id": int(teacher_id)})
+        teacher.update(user)
+        del teacher['_id']
+        results.append(teacher)
+    return jsonify({"teachers": results})
+
+@app.route("/create_user", methods=['GET', 'POST'])
+def create_user():
+    if request.method == 'POST':
+        print(request.form)
+        return "Se creó!"
+    else:
+        return render_template('createuser.html')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ server/db.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+client = MongoClient()
+db = client.test
+
+
+users = [{"name":"Alejandra", "last_name":'Contreras', 'cellnumber':'+56968788625', 'career':'Ingenieria Civil Industrial', 'id':0},
+         {"name":"Felipe", "last_name":'Pezao', 'cellnumber':'+56998261204', 'career':'Ingenieria Civil Computacion', 'id':1},
+         {"name":"Francisca", "last_name":'Rios', 'cellnumber':'+5688798323', 'career':'Ingenieria Civil Mecanica', 'id':2},
+         {"name":"Andres", "last_name":'Bustos', 'cellnumber':'+56999798775', 'career':'Ingenieria Civil Matematica', 'id':3},
+         {"name":"Catalina", "last_name":"Poblete", "cellnumber":"+56931221358", "career":"Ingenieria Civil Industrial", "id":4}]
+
+teachers = [{'id':3, 'curriculum':'He sido ayudante de MAT1610, MAT1620 y MAT1630.'},
+            {'id':0, 'curriculum':'Actual ayudante de MAT1640 y MAT1203'}]
+
+courses = [{'sigla':'MAT1610', 'name':'Calculo I', 'teachers':[3]},
+           {'sigla':'MAT1620', 'name':'Calculo II', 'teachers':[3]},
+           {'sigla':'MAT1630', 'name':'Calculo III', 'teachers':[3]},
+           {'sigla':'MAT1640', 'name':'Ecuaciones Diferenciales', 'teachers':[0]},
+           {'sigla':'MAT1203', 'name':'Algebra Lineal', 'teachers':[0]}]
+
+def fill_db():
+    for user in users:
+        db.users.insert_one(user)
+    for teacher in teachers:
+        db.teachers.insert_one(teacher)
+    for course in courses:
+        db.courses.insert_one(course)
+
+#a = db.users.find()
+#db.users.delete_many({'name':'Alejandra'})
+
+def delete_db():
+    pass
+
+if __name__ == '__main__':
+    fill_db()
